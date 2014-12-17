86dccda9ef9f498791e23a05ce91953a9d9cb040
Matt <matt@goreport.net>
2014-12-17 22:32:08 +0000

    remove text in top left

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -57,7 +57,7 @@
 		<nav id="topbar" role="navigation" style="margin-bottom: 0; z-index: 2;" class="navbar navbar-default navbar-static-top">
 			<div class="navbar-header">
 				<button type="button" data-toggle="collapse" data-target=".sidebar-collapse" class="navbar-toggle"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-				<a id="logo" href="/" class="navbar-brand"><span class="fa fa-rocket"></span><span class="logo-text">mobstar</span></a>
+
 			</div>
 			<div class="topbar-main"><a id="menu-toggle" href="#" class="hidden-xs"><i class="fa fa-bars"></i></a>
 				<ul class="nav navbar navbar-top-links navbar-right mbn">
