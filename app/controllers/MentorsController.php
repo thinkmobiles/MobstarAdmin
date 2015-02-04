590ca9c132fac317cca95ae256a3e13d3176994f
Anil Solanki <anil@spaceotechnologies.com>
2015-02-04 12:01:47 +0530

    mentor image upload changes

--- app/controllers/MentorsController.php
+++ app/controllers/MentorsController.php
@@ -1,5 +1,8 @@
 <?php
 
+use Aws\S3\S3Client;
+use Aws\Common\Credentials\Credentials as Creds;
+
 class MentorsController extends BaseController {
 
 	public function showMentors()
@@ -57,9 +60,47 @@ class MentorsController extends BaseController {
 		$this->data['mentor']->mentor_surname 		= $_POST['mentor_surname'];
 		$this->data['mentor']->mentor_bio 			= $_POST['mentor_bio'];
 
+		
 		if(empty($this->data['errors']))
 		{
 			$this->data['mentor']->save();
+
+			$profile = Input::file( 'mentor_profile_picture' );
+
+			if( !empty( $profile ) )
+			{
+				$destinationPath = 'profile/';
+
+				$file_in = $profile->getRealPath();
+
+				$file_out = $destinationPath.$this->data['mentor']->mentor_id . "-" . str_random( 12 ). ".jpg";
+
+				$img = Image::make( $file_in );
+
+				$img->resize( 200, 200 );
+
+				$img->save( $file_out, 80 );
+				
+				/*if(!is_dir($destinationPath))
+					$result = File::makeDirectory($destinationPath, 0777, true);
+				*/
+				//$profile->move($destinationPath, $filename);
+
+				if(isset($_POST['mentor_id']) && $_POST['mentor_id'] != '0')
+				{
+					if(File::exists($destinationPath.$this->data['mentor']->mentor_profile_picture))
+						File::delete($destinationPath.$this->data['mentor']->mentor_profile_picture);
+				}
+
+				$this->data['mentor']->mentor_profile_picture =  $file_out;	
+
+				$handle = fopen( $file_out , "r" );
+
+				Flysystem::connection( 'awss3' )->put( $file_out , fread( $handle,filesize( $file_out ) ) );		
+
+				$this->data['mentor']->save();
+			}
+
 			return Redirect::to('mentor/'.$this->data['mentor']->mentor_id);
 		}
 		else
@@ -67,11 +108,7 @@ class MentorsController extends BaseController {
 			return View::make('mentors/edit')->with('data', $this->data);
 		}
 		
-		
-
-
-
-		if(empty($this->data['errors']))
+		/*if(empty($this->data['errors']))
 		{
 			$this->data['category']->save();
 			return Redirect::to('category/'.$this->data['category']->category_id);
@@ -79,7 +116,7 @@ class MentorsController extends BaseController {
 		else
 		{
 			return View::make('categories/edit')->with('data', $this->data);
-		}
+		}*/
 	}
 
 	public function deleteMentor()
