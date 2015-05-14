49f07ca618d9666244624154ebd9192e814a81f6
spaceoanil <anil@spaceotechnologies.com>
2015-05-14 18:55:35 +0530

    changes

--- app/routes.php
+++ app/routes.php
@@ -89,6 +89,5 @@ Route::group(array('before' => 'auth'), function()
 	Route::post('comment/delete', array('uses' => 'CommentsController@commetDelete'));
 	Route::post('comment/savecomment', array('uses' => 'CommentsController@saveComment'));
 	
-	//Route::post('sendMessages', array('uses' => 'SendMessagesController@send'));
+	Route::post('sendMessages', array('uses' => 'SendMessagesController@send'));
 });
-Route::post('sendMessages', array('uses' => 'SendMessagesController@send'));
