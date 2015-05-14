bd0ab18ca35101e6554cf658ec095e62306f482d
spaceoanil <anil@spaceotechnologies.com>
2015-05-14 18:23:06 +0530

    changes

--- app/controllers/SendMessagesController.php
+++ app/controllers/SendMessagesController.php
@@ -3,7 +3,23 @@ class SendMessagesController extends BaseController {
 
 	public function send()
 	{
-		$recipient = User::where('user_deleted', '=', 0)->get();
+		
+		$this->data['sidenav']['messages']['page_selected'] = true;
+
+		$this->data['errors'] = array();
+				
+		if(empty($_POST['message']))
+		{
+			$this->data['errors']['message'] = "Message can't be left blank.";
+		}
+		$recipArray = [ ];
+		$particArray = [ ];
+		$newThread = '';
+		$message_group = 0;
+		if($message_group == 0)
+		{
+			$recipient = DB::table('users')->where('user_deleted', '=', 0)->take(10);
+		}
 		print_r($recipient);
 	}
 }
