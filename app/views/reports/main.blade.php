5779ecb99f838ced0c054e7316d4cacd1d7452ba
spaceoanil <anil@spaceotechnologies.com>
2015-06-26 15:55:35 +0530

    changes

--- app/views/reports/main.blade.php
+++ app/views/reports/main.blade.php
@@ -21,7 +21,7 @@
 					</a>
 					@endforeach
 				</div>
-				<?php echo (!isset($_GET['showAll'])) ? $data['tags']->links() : ""; ?>
+				<?php echo (!isset($_GET['showAll'])) ? $data['reports']->links() : ""; ?>
 			</div>
 		</div>
 	</div>
